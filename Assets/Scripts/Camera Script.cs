e5f782b034578f58a356dcaf381756ac27b571d9
Jakob Lagle <87400188+Juneyspy@users.noreply.github.com>
2023-04-03 07:45:20 -0400

    stuff

---
+++ Assets/Scripts/Camera Script.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("OverworldMC");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = player.gameObject.transform.position + new Vector3(0,0,-12.11325f);
+    }
+}
