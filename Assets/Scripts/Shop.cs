a75758889deb45bc08262a8452a37cf81de62bc0
KingOfTitans09 <natescars@icloud.com>
2023-04-10 08:04:09 -0400

    Added more to shop script

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
+    public GameObject shopUI;
+    public GameObject shopNPC;
+    public GameObject player;
+    float angle = 180;
     // Start is called before the first frame update
     void Start()
     {
-        
+        shopUI.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Vector3.Angle(player.transform.forward, transform.position - player.transform.position) < angle)
+        {
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                shopUI.SetActive(true);
+            }
+            else if (shopUI.activeInHierarchy && Input.GetKeyUp(KeyCode.B))
+            {
+                shopUI.SetActive(false);
+            }
+        }
     }
 }
