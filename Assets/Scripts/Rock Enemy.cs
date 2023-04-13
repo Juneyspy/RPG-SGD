6d2942c813ab10a92361f2329612c56350ad7a6e
Jakob Lagle <87400188+Juneyspy@users.noreply.github.com>
2023-04-13 10:21:43 -0400

    aaa

--- Assets/Scripts/Rock Enemy.cs
+++ Assets/Scripts/Rock Enemy.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RockEnemy : MonoBehaviour
 {
     public GameObject player;
     public GameObject chest;
+    public GameObject fightMenu;
+    public GameObject fadingGO;
+    public Image fading;
 
     // Start is called before the first frame update
     void Start()
     {
+        //fightMenu = GameObject.Find("fightingcanvas"); fightMenu.SetActive(false);
+        fightMenu = GameObject.Find("fightscene"); fightMenu.SetActive(false);
         player = GameObject.Find("OverwoldMC");
         chest = GameObject.Find("Chest - Wooden");
+        fadingGO = GameObject.Find("Fading");
+        fading = fadingGO.GetComponent<Image>(); fading.CrossFadeAlpha(0,0.0f,true);
     }
 
     // Update is called once per frame
@@ -23,6 +31,8 @@ public class RockEnemy : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Player"){
             if(chest.GetComponent<WoodenDrops>().opened){
+                fading.CrossFadeAlpha(1,1.0f,true);
+                //fightMenu.SetActive(true);
                 print("initiate fight");
             }
             else{
