e7b9f85d6109ff30e6382093e410dd7d38777dfe
Jakob Lagle <87400188+Juneyspy@users.noreply.github.com>
2023-04-14 09:15:14 -0400

    fight things

--- Assets/Scripts/Rock Enemy.cs
+++ Assets/Scripts/Rock Enemy.cs
@@ -10,16 +10,20 @@ public class RockEnemy : MonoBehaviour
     public GameObject fightMenu;
     public GameObject fadingGO;
     public Image fading;
+    public Image finalFade;
 
     // Start is called before the first frame update
     void Start()
     {
         //fightMenu = GameObject.Find("fightingcanvas"); fightMenu.SetActive(false);
-        fightMenu = GameObject.Find("fightscene"); fightMenu.SetActive(false);
-        player = GameObject.Find("OverwoldMC");
-        chest = GameObject.Find("Chest - Wooden");
+        fadingGO = GameObject.Find("FinalFade");
+        finalFade = fadingGO.GetComponent<Image>(); finalFade.CrossFadeAlpha(0,0.0f,true);
         fadingGO = GameObject.Find("Fading");
         fading = fadingGO.GetComponent<Image>(); fading.CrossFadeAlpha(0,0.0f,true);
+
+        fightMenu = GameObject.Find("fighting sex"); fightMenu.SetActive(false);
+        player = GameObject.Find("OverwoldMC");
+        chest = GameObject.Find("Chest - Wooden");
     }
 
     // Update is called once per frame
@@ -31,13 +35,33 @@ public class RockEnemy : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Player"){
             if(chest.GetComponent<WoodenDrops>().opened){
-                fading.CrossFadeAlpha(1,1.0f,true);
-                //fightMenu.SetActive(true);
+                fading.CrossFadeAlpha(1,.2f,true);
+                Invoke("fadeOut",.2f);
+                Invoke("fadeIn",.4f);
+                Invoke("fadeOut",.6f);
+                Invoke("fadeIn",.8f);
+                Invoke("TurnOnFighting",.8f);
+                Invoke("fadeOutFinal",1.0f);
+                Invoke("fadeOut",1.0f);
                 print("initiate fight");
             }
             else{
                 print("i recommend you open the chest first");
             }
         }
+    }
+
+    public void fadeOut(){
+        fading.CrossFadeAlpha(0,.2f,true);
+    }
+    public void fadeIn(){
+        fading.CrossFadeAlpha(1,.2f,true);
+    }
+    public void TurnOnFighting(){
+        fightMenu.SetActive(true);
+        finalFade.CrossFadeAlpha(1,0.0f,true);
+    }
+    public void fadeOutFinal(){
+        finalFade.CrossFadeAlpha(0,.2f,true);
     }
 }
