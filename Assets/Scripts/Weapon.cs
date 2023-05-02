8e9478dcac70be6a7cc61dc94e08b9889f42a481
Ijstoops <89106790+Ijstoops@users.noreply.github.com>
2023-05-02 10:06:57 -0400

    quick update no worry :+1:

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -11,12 +11,70 @@ public class Weapon : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //if in inventory slot 1 or something:
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
     }
+
+    public void Sword1works()
+    {
+        //no rune slots, only stats
+    }
+
+    public void Sword2works()
+    {
+        //no rune slots, only stats
+    }
+
+
+    public void Sword3works()
+    {
+        GameObject rune1;
+
+        //check for runes and figure out how to put runes on an item
+        //figure out how to get runes from the slots in the rune smith screen
+
+    }
+
+    public void Sword4works()
+    {
+        GameObject rune1;
+
+    }
+
+    public void Sword5works()
+    {
+        GameObject rune1;
+        GameObject rune2;
+
+    }
+
+    public void Sword6works()
+    {
+        GameObject rune1;
+        GameObject rune2;
+
+    }
+
+    public void Sword7works()
+    {
+        GameObject rune1;
+        GameObject rune2;
+        GameObject rune3;
+
+    }
+
+    public void Sword8works()
+    {
+        GameObject rune1;
+        GameObject rune2;
+        GameObject rune3;
+    }
+
 }
