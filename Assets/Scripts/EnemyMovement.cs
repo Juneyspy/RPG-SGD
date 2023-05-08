d76cfc393148d0a8edd3d8c8daf9ca89d052d5e6
KingOfTitans09 <natescars@icloud.com>
2023-05-08 10:16:08 -0400

    Started coding enemy movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public Transform[] target;
+    public float speed;
+    private int current;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position != target[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else current = (current + 1) % target.Length;
     }
 }
